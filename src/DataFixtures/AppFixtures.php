d179b19bde639f64c0c412c83de15c78a3d77a48
mano_lis <laptit99@hotmail.fr>
2022-05-12 14:47:37 +0200

    before loading fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\BookFactory;
+use App\Factory\BorrowFactory;
+use App\Factory\PhysicalBookFactory;
+use App\Factory\ReviewFactory;
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,6 +14,17 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        UserFactory::createMany(40);
+
+        BookFactory::createMany(100);
+
+        PhysicalBookFactory::createMany(150);
+
+        ReviewFactory::createMany(30);
+
+        BorrowFactory::createMany(50);
+        
+        
         // $product = new Product();
         // $manager->persist($product);
 
